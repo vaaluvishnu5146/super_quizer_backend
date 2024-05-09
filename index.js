e1866ad3e1958c2fe596f30510546ad53aee0544
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-05-09 19:04:28 +0530

    Added Env configs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const http_server = express();
 
-const PORT = 5000;
-const HOSTNAME = "localhost";
+// Configure DOTENV
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const HOSTNAME = process.env.HOSTNAME;
 
 http_server.listen(PORT, HOSTNAME, () => {
   console.log(`Server started at http://${HOSTNAME}:${PORT}`);
