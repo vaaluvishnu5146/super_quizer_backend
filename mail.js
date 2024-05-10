85a3d9063acf7de8f85aa45c1d05679da3dbb077
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-05-10 20:55:19 +0530

    Added Nodemailer config

---
+++ mail.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "dino78@ethereal.email",
+    pass: "ABJumEnJFzf7nd1Ah5",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Dino 👻" <dino78@ethereal.email>', // sender address
+    to: "", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
